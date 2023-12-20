1fef21dea687480cfe8a6307fb761eb7644c780e
George G <georgegatuma.gg@gmail.com>
2023-12-20 23:53:40 +0300

    init project

---
+++ src/duct_basic_server/handler/example.clj
@@ -0,0 +1,8 @@
+(ns duct-basic-server.handler.example
+  (:require [compojure.core :refer :all]
+            [integrant.core :as ig]))
+
+(defmethod ig/init-key :duct-basic-server.handler/example [_ options]
+  (context "/example" []
+    (GET "/" []
+      {:body {:example "data"}})))
